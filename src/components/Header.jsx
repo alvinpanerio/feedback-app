13f59f7bf969ec7dfd00ddb1ad0b5629ee47037f
alvinpanerio <panerio.alvin18@gmail.com>
2023-02-20 15:32:48 +0800

    Added styles/components/props

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "../index.css";
+import PropTypes from "prop-types";
+
+function Header({ title }) {
+  return (
+    <header>
+      <div className="container">{title}</div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  title: "Feedback App", 
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Header;
