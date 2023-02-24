741904cd8f3a6171ca48bbc901ba6922906f96f2
alvinpanerio <panerio.alvin18@gmail.com>
2023-02-24 15:06:22 +0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import About from "./components/pages/About";
+import AboutIcon from "./components/AboutIcon";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <>
+    <Router>
       <Header />
-
       <div className="container">
-        <FeedbackForm
-          addFeedback={(obj) => {
-            setFeedback([...feedback, obj]);
-          }}
-        />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList
-          feedback={feedback}
-          handleDelete={(id) =>
-            setFeedback(feedback.filter((i) => i.id !== id))
-          }
-        />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm
+                  addFeedback={(obj) => {
+                    setFeedback([...feedback, obj]);
+                  }}
+                />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={(id) =>
+                    setFeedback(feedback.filter((i) => i.id !== id))
+                  }
+                />
+                <AboutIcon />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
